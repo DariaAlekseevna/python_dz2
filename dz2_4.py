a27f99edce74499256e9fda0d2b58cadc6844496
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-09-25 09:29:19 +0300

    добавлены решения задач

--- dz2_4.py
+++ dz2_4.py
@@ -1,2 +1,20 @@
 #Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 #Найдите произведение элементов на указанных пользователем через пробел позициях.
+
+from random import random
+from random import randint
+
+N = int(input('введите число: '))
+N_list = []
+for i in range(N):
+    N_list.append(randint(-N, N))
+print(N_list)
+
+num = int(input('введите количество позиций: '))
+num_list = []
+sum = 1
+for j in range(num):
+    num_list.append(int(input("введите позицию элемента в строке: ")))
+    sum = sum * N_list[num_list[j]]
+print(f'произведение элементов на указанных пользователем позициях: {sum}')
+
--- dz2_5.py
+++ dz2_5.py
@@ -1,1 +1,14 @@
 #Реализуйте алгоритм перемешивания списка.
+from random import random, shuffle
+
+num = int(input('введите количество элементов в списке: '))
+num_list = []
+for i in range(num):
+    num_list.append(input("введите элемент в списке: "))
+
+print("список заданный пользователем: ", num_list)
+
+shuffle(num_list)
+print('перемешанный список: ', num_list)
+
+
